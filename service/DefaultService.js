531468171afc1b6c9e7e07dd334f9407df186ee0
akshayTrustG <akshayk@trustgrid.com>
2025-07-01 16:45:16 +0530

    example updated

--- service/DefaultService.js
+++ service/DefaultService.js
@@ -11,24 +11,39 @@
 exports.resolve = function(identifier,accept) {
   return new Promise(function(resolve, reject) {
     var examples = {};
-    examples['did:trustgrid:0000000000123456'] = {
-      "@context": "https://www.w3.org/2019/did/v1",
-      "id" : "did:trustgrid:0000000000123456",
-      "publicKey" : [ {
-        "id" : "did:trustgrid:0000000000123456#key-1",
-        "type" : "Ed25519VerificationKey2018",
-        "publicKeyBase58" : "H3C2AVvLMv6gmMNam3uVAjZpfkcJCwDwnZn6z3wXmqPV"
-      } ]
+    // examples['did:tgrid:trustgrid:dev:QjA1qdXKmxzgK4u8mFoBpF'] = {
+    //   "@context": "https://www.w3.org/2019/did/v1",
+    //   "id" : "did:trustgrid:0000000000123456",
+    //   "publicKey" : [ {
+    //     "id" : "did:trustgrid:0000000000123456#key-1",
+    //     "type" : "Ed25519VerificationKey2018",
+    //     "publicKeyBase58" : "H3C2AVvLMv6gmMNam3uVAjZpfkcJCwDwnZn6z3wXmqPV"
+    //   } ]
+    // };
+    examples['did:tgrid:trustgrid:dev:QjA1qdXKmxzgK4u8mFoBpF'] = {
+      "didDocument": {
+        "@context": "https://www.w3.org/ns/did/v1",
+        "id": "did:tgrid:trustgrid:dev:QjA1qdXKmxzgK4u8mFoBpF",
+        "verificationMethod": [
+          {
+            "id": "did:tgrid:trustgrid:dev:QjA1qdXKmxzgK4u8mFoBpF#key-1",
+            "type": "Ed25519VerificationKey2018",
+            "controller": "did:tgrid:trustgrid:dev:QjA1qdXKmxzgK4u8mFoBpF",
+            "publicKeyBase58": "Dw3zBGuiy84bQSXfKLZ7LpmifJiTSdWHZSSUUHAH2Fvk"
+          }
+        ],
+        "authentication": [
+          "did:tgrid:trustgrid:dev:QjA1qdXKmxzgK4u8mFoBpF#key-1"
+        ]
+      },
+      "didDocumentMetadata": {
+    
+      },
+      "didResolutionMetadata": {
+        "contentType": "application/did+ld+json"
+      }
     };
-    examples['did:trustgrid:0000000000456789'] = {
-      "@context": "https://www.w3.org/2019/did/v1",
-      "id" : "did:trustgrid:0000000000456789",
-      "publicKey" : [ {
-        "id" : "did:trustgrid:0000000000456789#key-1",
-        "type" : "Ed25519VerificationKey2018",
-        "publicKeyBase58": "EKmHWjiDDeMhEBs75uv86dC89zZ6yTTxEAHkYroJ7wZP"
-      } ]
-    };
+
     var found = examples[identifier];
     if (found) {
       resolve(found);
